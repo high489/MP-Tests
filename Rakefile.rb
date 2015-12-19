16f5e6e2535b9ccd66402bbe58aa0db151c4a005
Admin <admin@Mac-Admin.local>
2015-12-19 05:45:39 +0300

    Update rake file (report and screenshots paths are changed)

--- Rakefile.rb
+++ Rakefile.rb
@@ -5,7 +5,7 @@ require 'rubygems'
 
 
 #Run cucumber tests
-Cucumber::Rake::Task.new :features do |t|
+Cucumber::Rake::Task.new :run_ios_tests do |t|
  # settings_file = Dir.pwd + '/config/cucumber.yml'
   settings_file = 'config/cucumber.yml'
   configs = YAML.load_file settings_file
@@ -21,19 +21,16 @@ Cucumber::Rake::Task.new :features do |t|
   # Get UDID
   udid = ENV.has_key?('TEST_IOS_UDID') ? ENV['TEST_IOS_UDID'] : configs['ios_udid']
 
-  # Create folders for report and screenshots
-  report_dir = ENV.has_key?('TEST_UTILS_REPORT_DIR') ? ENV['TEST_UTILS_REPORT_DIR'] : configs['report_dir_absolute_path']
-  screenshot_dir = ENV.has_key?('TEST_UTILS_SCREENSHOT_DIR') ? ENV['TEST_UTILS_SCREENSHOT_DIR'] : configs['screenshot_dir_absolute_path']
+  # Create folder for and screenshots
+  screenshot_dir= "./screenshots/"
 
   FileUtils.rmtree(screenshot_dir)
-  FileUtils.rmtree(report_dir)
-  FileUtils.mkdir_p(report_dir)
   FileUtils.mkdir_p(screenshot_dir)
 
     t.cucumber_opts = [
                       "features/",
                       "--format progress",
-                      "--format html -o #{report_dir}/tests_result.html",
+                      "--format html -o ./report.html",
                       "DEVICE_TARGET='#{udid}' ",
                       "APP_BUNDLE_PATH='#{app_name_with_path}' ",
                       "SCREENSHOT_PATH='#{screenshot_dir}' "
