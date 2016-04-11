475c0be3b206a5b073e7e91e68779081023dbd52
high489 <high489@gmail.com>
2016-04-12 02:33:12 +0300

    Change access code

--- features/ios/pages/access_code_page.rb
+++ features/ios/pages/access_code_page.rb
@@ -5,12 +5,12 @@ class AccessCodePage < Calabash::IBase
   end
 
   def enter_access_code
-    touch("button marked:'4'")
     touch("button marked:'1'")
-    touch("button marked:'6'")
-    touch("button marked:'6'")
+    touch("button marked:'0'")
+    touch("button marked:'7'")
+    touch("button marked:'1'")
+    touch("button marked:'7'")
     touch("button marked:'3'")
-    touch("button marked:'9'")
     touch("button index:11")
   end
 
